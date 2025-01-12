abca4fadeb59b0595698c56e596faf60aa67e4a2
Veeraiah <harshithveeraiah@csus.edu>
2025-01-12 13:09:36 -0800

    Day02-03

---
+++ Day-02/Max_Number_of_K_Sum_Pair.java
@@ -0,0 +1,33 @@
+// You are given an integer array nums and an integer k.
+
+// In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+// Return the maximum number of operations you can perform on the array.
+
+// Example 1:
+// Input: nums = [1,2,3,4], k = 5
+// Output: 2
+// Explanation: Starting with nums = [1,2,3,4]:
+// - Remove numbers 1 and 4, then nums = [2,3]
+// - Remove numbers 2 and 3, then nums = []
+// There are no more pairs that sum up to 5, hence a total of 2 operations.
+
+
+
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+
+        for (int num : nums) {
+            int complement = k - num;
+            if (map.getOrDefault(complement, 0) > 0) {
+                count++;
+                map.put(complement, map.get(complement) - 1);
+            } else {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+        }
+        return count;
+    }
+}
